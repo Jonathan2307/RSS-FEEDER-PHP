bd104d03fa870878acfa99df5d91911a83122425
Jonathan2307 <gratigny.j@me.com>
2021-07-01 09:13:05 +0200

    add algo

--- index.php
+++ index.php
@@ -1,17 +1,48 @@
 <?php
+$xml = simplexml_load_file('https://www.lemonde.fr/international/rss_full.xml');
+$xml1 = simplexml_load_file('https://www.lemonde.fr/societe/rss_full.xml');
+$xml2 = simplexml_load_file('https://www.lemonde.fr/economie/rss_full.xml');
+$xml3 = simplexml_load_file('https://www.lemonde.fr/sport/rss_full.xml');
+$xml4 = simplexml_load_file('https://www.lemonde.fr/cultures-web/rss_full.xml');
 
-simplexml_load_file('https://www.lemonde.fr/international/rss_full.xml', )
+
+function getRssFlow($xml)
+{
+    foreach ($xml->channel->item as $article) {
+        echo $article->title . '<br>';
+        echo $article->description . '<br>';
+        echo $article->link . '<br>';
+        echo $article->children('media', true)->content->attributes() . '<br>';
+        echo $article->children('media', true)->content->description . '<br>';
+    }
+};
+
+function getRssXtimes($xml)
+{
+    for ($i = 0; $i < 10; $i++) {
+        echo $xml->channel->item[$i]->title . '<br>';
+        echo $xml->channel->item[$i]->description . '<br>';
+        echo $xml->channel->item[$i]->link . '<br>';
+        echo $xml->channel->item[$i]->children('media', true)->content->attributes() . '<br>';
+        echo $xml->channel->item[$i]->children('media', true)->content->description . '<br>';
+    }
+};
+
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
